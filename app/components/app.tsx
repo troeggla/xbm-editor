270797602f266356d0fe9d2123d65e0ed7839777
Thomas Röggla <tom@lichtfragmente.com>
2019-12-15 23:20:39 +0100

    drawing fixed pixel grid using CSS grid

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { useState } from "react";
+
+import Pixel from "./pixel";
 
 function initMatrix(dimensions: [number, number]): boolean[][] {
   const [width, height] = dimensions;
@@ -16,8 +19,34 @@ function initMatrix(dimensions: [number, number]): boolean[][] {
 }
 
 const App: React.FC = () => {
+  const [dimensions, setDimensions] = useState<[number, number]>([32, 32]);
+  const [grid, setGrid] = useState<boolean[][]>(initMatrix(dimensions));
+
+  const togglePixel = (x: number, y: number) => {
+    const tmpGrid = grid.slice();
+    tmpGrid[x][y] = !tmpGrid[x][y];
+
+    setGrid(tmpGrid);
+  };
+
+  const [width, height] = dimensions;
+  const gridTemplate = `repeat(${height}, 10px) / repeat(${width}, 10px)`;
+
   return (
     <div id="app">
+      <div style={{ display: "grid", gridTemplate }}>
+        {grid.map((col, x) => {
+          return col.map((filled, y) => {
+            return (
+              <Pixel
+                key={`${x}.${y}`}
+                filled={filled}
+                onUpdate={togglePixel.bind(null, x, y)}
+              />
+            );
+          });
+        })}
+      </div>
     </div>
   );
 };

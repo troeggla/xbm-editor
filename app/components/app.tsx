62851b7ff79c58dfa303aa8baf99f51d4370b88c
Thomas Röggla <tom@lichtfragmente.com>
2019-12-15 23:19:48 +0100

    added function for initialising a boolean matrix

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -1,5 +1,20 @@
 import * as React from "react";
 
+function initMatrix(dimensions: [number, number]): boolean[][] {
+  const [width, height] = dimensions;
+  const matrix: boolean[][] = [];
+
+  for (let x = 0; x < width; x++) {
+    matrix[x] = [];
+
+    for (let y = 0; y < height; y++) {
+      matrix[x][y] = false;
+    }
+  }
+
+  return matrix;
+}
+
 const App: React.FC = () => {
   return (
     <div id="app">

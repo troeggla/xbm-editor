85bfc819eb75339f9aeadfb6905ae52bd87097c1
Thomas Röggla <tom@lichtfragmente.com>
2019-12-23 01:14:40 +0100

    added some styling for the controls component

--- app/components/controls.tsx
+++ app/components/controls.tsx
@@ -15,35 +15,34 @@ const Controls: React.FC<ControlsProps> = (props) => {
   const { cellSize, dimensions, onCellSizeUpdated, onDimensionsUpdated } = props;
 
   return (
-    <div>
-      Cell size:&emsp;
-      <input
-        type="number"
-        value={cellSize}
-        min={0}
-        onChange={(e) => onCellSizeUpdated(e.target.valueAsNumber)}
-      />
+    <div className="controls">
+      <div>
+        Cell size:&emsp;
+        <input
+          type="number"
+          value={cellSize}
+          min={0}
+          onChange={(e) => onCellSizeUpdated(e.target.valueAsNumber)}
+        />
+      </div>
 
-      <br />
-
-      Dimensions:&emsp;
-      <input
-        type="number"
-        value={dimensions[0]}
-        min={1}
-        max={MAX_SCREEN_WIDTH}
-        onChange={(e) => onDimensionsUpdated([e.target.valueAsNumber, dimensions[1]])}
-      />
-      <input
-        type="number"
-        value={dimensions[1]}
-        min={1}
-        max={MAX_SCREEN_HEIGHT}
-        onChange={(e) => onDimensionsUpdated([dimensions[0], e.target.valueAsNumber])}
-      />
-
-      <br />
-      <br />
+      <div>
+        Dimensions:&emsp;
+        <input
+          type="number"
+          value={dimensions[0]}
+          min={1}
+          max={MAX_SCREEN_WIDTH}
+          onChange={(e) => onDimensionsUpdated([e.target.valueAsNumber, dimensions[1]])}
+        />
+        <input
+          type="number"
+          value={dimensions[1]}
+          min={1}
+          max={MAX_SCREEN_HEIGHT}
+          onChange={(e) => onDimensionsUpdated([dimensions[0], e.target.valueAsNumber])}
+        />
+      </div>
     </div>
   );
 };

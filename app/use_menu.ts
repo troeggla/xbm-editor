ef991e2cf5decceef0335a7e6f102c5d9cef3366
Thomas Röggla <tom@lichtfragmente.com>
2020-01-03 00:08:12 +0100

    extracted grid transformations into separate functions

--- app/use_menu.ts
+++ app/use_menu.ts
@@ -3,6 +3,18 @@ import { ipcRenderer as ipc } from "electron-better-ipc";
 
 import { initGrid, getGridDimensions } from "./util";
 
+type GridTransformation = (grid: boolean[][]) => boolean[][];
+
+const invertGrid: GridTransformation = (grid) => {
+  return grid.map((col) => {
+    return col.map((pixel) => !pixel);
+  });
+};
+
+const clearGrid: GridTransformation = (grid) => {
+  return initGrid(getGridDimensions(grid));
+};
+
 function useMenu(grid: boolean[][], setGrid: (grid: boolean[][]) => void) {
   const gridRef = useRef<boolean[][]>(grid);
 
@@ -17,11 +29,9 @@ function useMenu(grid: boolean[][], setGrid: (grid: boolean[][]) => void) {
 
       switch (itemId) {
         case "invert":
-          return setGrid(grid.map((col) => {
-            return col.map((pixel) => !pixel);
-          }));
+          return setGrid(invertGrid(grid));
         case "clear":
-          return setGrid(initGrid(getGridDimensions(grid)));
+          return setGrid(clearGrid(grid));
         default:
           return grid;
       }

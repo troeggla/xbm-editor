a8e38d9cd88cdcefd9943c08c11de1c3981c5fe9
Thomas Röggla <tom@lichtfragmente.com>
2021-12-23 01:04:58 +0100

    added webpack config for preload script

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,31 @@ const rendererTarget = {
   }
 };
 
+const preloadTarget = {
+  entry: "./preload/main.ts",
+  mode: "development",
+  target: "electron-main",
+  output: {
+    path: __dirname + "/bundle",
+    filename: "preload.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  node: {
+    __dirname: false
+  }
+}
+
 const mainProcessTarget = {
   entry: "./main/main.ts",
   mode: "development",
@@ -51,4 +76,4 @@ const mainProcessTarget = {
   }
 };
 
-module.exports = [rendererTarget, mainProcessTarget];
+module.exports = [rendererTarget, preloadTarget, mainProcessTarget];
